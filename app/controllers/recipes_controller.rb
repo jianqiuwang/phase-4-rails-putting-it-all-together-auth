486f4e219058df9ef58bf010727c62726ee9be11
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-16 19:34:42 -0500

    commit tree 30cc2bb93478c15faf86c7ba2ecf48792be27d98

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,28 @@
+# require 'pry'
+class RecipesController < ApplicationController
+    before_action :authorize
+    def index
+        recipes = Recipe.all
+        render json: recipes
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        recipe = user.recipes.create(recipe_params) 
+        if recipe.valid?
+            render json: recipe, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+   private
+
+    def authorize
+        return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+    end
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    before_action :authorized, only: [:show]
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        current_user = User.find_by(id: session[:user_id])
+        render json: current_user
+    end
+    
+    private
+    
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    def authorized
+        return render json: {errors: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
+    end
+end
